19154571d0f422404d67a82499350d8a61114d2a
Shalom Bhooshi <s.bhooshi@gmail.com>
2018-07-08 19:10:02 +0200

    Add Travis CI tests skeleton

---
+++ tests/env-prep.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -eu
+set -xv
+
+# setup
+if type -P apt; then
+  apt-get -qq -y update
+  apt-get install -y --no-install-recommends \
+    ca-certificates curl dbus python-minimal python-urllib3
+
+elif type -P zypper; then
+  zypper -n install curl python python-xml
+
+elif type -P dnf; then
+  dnf install -y curl python
+
+elif type -P yum; then
+  source /etc/os-release
+  if [[ $ID = rhel ]]; then
+    curl http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
+      -o /tmp/epel.rpm
+    rpm -ivh /tmp/epel.rpm
+    yum --disablerepo=* --enablerepo=epel install -y python-pip
+  else
+    yum install -y curl python
+  fi
+
+fi
+
+if type -P pip; then
+  pip install --upgrade pip
+else
+  curl https://bootstrap.pypa.io/get-pip.py | python
+fi
+
+pip install --upgrade ansible
+
